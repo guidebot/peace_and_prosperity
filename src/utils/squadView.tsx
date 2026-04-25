6f9f6f5660f354447f03bfb5e58ed1bb60538350
Nikita Makarov <5253450@gmail.com>
2026-04-25 09:08:15 +0300

    Таблица навыков фиксирована

--- src/utils/squadView.tsx
+++ src/utils/squadView.tsx
@@ -4,12 +4,30 @@ import { Entity } from '../game/Entity';
 import Handlebars from 'handlebars';
 import template from './squadTemplate.hbs?raw';
 
-Handlebars.registerHelper('gt', function(a, b) {
+Handlebars.registerHelper('gt', function (a, b) {
   return a > b;
 });
 
 const compiledTemplate = Handlebars.compile(template);
 
+function padTo<T>(arr: T[], size: number, filler: T): T[] {
+  const result = [...arr];
+  while (result.length < size) {
+    result.push(filler);
+  }
+  return result.slice(0, size);
+}
+
 export function generateSquadViewHTML(players: Player[]): string {
-  return compiledTemplate({ players });
+  const payload = players.map(player => {
+    const paddedUnits = player.children.map(unit => ({
+      ...unit,
+      children: padTo(unit.children || [], 13, new Entity("", {}, []))
+    }));
+    return {
+      ...player,
+      children: paddedUnits
+    };
+  });
+  return compiledTemplate({ players: payload });
 }
